e37ad3eefa52a8429b98c7cf152efb7fd87136c2
PedroVergilio <pedroavergilio@gmail.com>
2026-05-14 20:51:22 -0300

    chore: oculta chaves do firebase com variaveis de ambiente

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Aqui você colará a chave que o Firebase vai te dar no site deles
 const firebaseConfig = {
-  apiKey: "AIzaSyDFXGZ0psOOwSx4dGMfbQ4C53l-LMbFhN8",
-  authDomain: "lista-casa-nova-bd.firebaseapp.com",
-  projectId: "lista-casa-nova-bd",
-  storageBucket: "lista-casa-nova-bd.firebasestorage.app",
-  messagingSenderId: "321625966061",
-  appId: "1:321625966061:web:b89103d376f304a8c3d3e4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
